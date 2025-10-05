b525382a9625c76a88cf8b76f0c7bfc5cb8b4300
Luiz <luizzlh2004@gmail.com>
2025-10-05 12:37:18 -0300

    Adicionado classe ClienteDAO

--- src/main/java/br/com/DAO/ClienteDAO.java
+++ src/main/java/br/com/DAO/ClienteDAO.java
@@ -1,2 +1,35 @@
-package br.com.DAO;public class ClienteDAO {
+package br.com.DAO;
+
+import br.com.Repositorio.Conexao;
+import br.com.entidades.Cliente;
+
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class ClienteDAO {
+
+    public void cadastrarCliente(Cliente cliente){
+
+        String cadastrarUsuario = "INSERT INTO CLIENTES (NOME, IDADE, EMAIL, DEVEDOR) VALUES (?, ?, ?, ?)";
+
+        PreparedStatement preparedStatement = null;
+
+        try{
+            preparedStatement = Conexao.getConexao().prepareStatement(cadastrarUsuario);
+            preparedStatement.setString(1, cliente.getNome());
+            preparedStatement.setInt(2, cliente.getIdade());
+            preparedStatement.setString(3, cliente.getEmail());
+            preparedStatement.setInt(4, 0);
+
+            preparedStatement.execute();
+
+            preparedStatement.close();
+
+            System.out.println("Cliente cadastrado com sucessO!");
+        } catch(SQLException e){
+            e.printStackTrace();
+        }
+
+    }
+
 }
