25614c1acedccc0ed225e7dd53b3866f9ec9bd83
Luiz <luizzlh2004@gmail.com>
2025-10-05 11:55:10 -0300

    Adicionando Driver JDBC MySQL

---
+++ src/main/java/br/com/Repositorio/Conexao.java
@@ -0,0 +1,5 @@
+package br.com.Repositorio;
+
+public class Conexao {
+
+}
---
+++ src/main/java/br/com/livraria/Livraria.java
@@ -0,0 +1,15 @@
+package br.com.livraria;
+
+import br.com.entidades.Cliente;
+import br.com.entidades.Livro;
+import br.com.entidades.Pedido;
+
+public class Livraria {
+
+    private Livro livro;
+    private Cliente cliente;
+    private Pedido pedido;
+
+
+
+}
