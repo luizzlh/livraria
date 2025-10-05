890fd777062ed1fb3eab8b290d768baf20c9fef7
Luiz <luizzlh2004@gmail.com>
2025-10-05 12:36:12 -0300

    Correção na classe de conexão

--- src/main/java/br/com/Repositorio/Conexao.java
+++ src/main/java/br/com/Repositorio/Conexao.java
@@ -1,5 +1,29 @@
 package br.com.Repositorio;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Conexao {
 
+    private static final String url = "jdbc:mysql://localhost:3306/db_livraria";
+    private static final String user = "root";
+    private static final String password = "luiz1402";
+
+    private static Connection conn;
+
+    public static Connection getConexao() {
+
+        try{
+            if(conn == null){
+                conn = DriverManager.getConnection(url, user, password);
+                return conn;
+            }else{
+                return conn;
+            }
+        } catch(SQLException e){
+            e.printStackTrace();
+            return null;
+        }
+    }
 }
