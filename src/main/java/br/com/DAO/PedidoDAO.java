8350bea6fdec5e2c0a1787da187b7a4cee4d06de
Luiz <luizzlh2004@gmail.com>
2025-10-05 21:19:32 -0300

    Criado método de criar pedido

--- src/main/java/br/com/DAO/PedidoDAO.java
+++ src/main/java/br/com/DAO/PedidoDAO.java
@@ -1,2 +1,36 @@
-package br.com.DAO;public class PedidoDAO {
+package br.com.DAO;
+
+import br.com.Repositorio.Conexao;
+import br.com.entidades.Pedido;
+
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class PedidoDAO {
+    static Pedido pedido;
+
+    private static void criarPedido(int idCliente, int idLivro){
+
+        String queryInsertPedido = "INSERT INTO PEDIDOS (IDCLIENTE, IDLIVRO, DATAPEDIDO, STATUS) VALUES (?, ?, CURDATE(), ?)";
+
+        PreparedStatement preparedStatement = null;
+
+        try{
+            preparedStatement = Conexao.getConexao().prepareStatement(queryInsertPedido);
+            preparedStatement.setInt(1, idCliente);
+            preparedStatement.setInt(2, idLivro);
+            preparedStatement.setInt(4, pedido.getStatus());
+
+            preparedStatement.execute();
+
+            preparedStatement.close();
+
+            System.out.println("Pedido criado com sucesso!");
+
+        } catch(SQLException e){
+            System.out.println("Não foi possivel criar o pedido: ");
+            e.printStackTrace();
+        }
+    }
+
 }
