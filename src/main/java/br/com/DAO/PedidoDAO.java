91ebe7550f151c05fdd01d3a89c004cdf96c3d8d
Luiz <luizzlh2004@gmail.com>
2025-10-05 21:35:09 -0300

    Implementado método de pedido de devolução de livro

--- src/main/java/br/com/DAO/PedidoDAO.java
+++ src/main/java/br/com/DAO/PedidoDAO.java
@@ -9,9 +9,9 @@ import java.sql.SQLException;
 public class PedidoDAO {
     static Pedido pedido;
 
-    private static void criarPedido(int idCliente, int idLivro){
+    public static void criarPedidoAluguel(int idCliente, int idLivro){
 
-        String queryInsertPedido = "INSERT INTO PEDIDOS (IDCLIENTE, IDLIVRO, DATAPEDIDO) VALUES (?, ?, CURDATE())";
+        String queryInsertPedido = "INSERT INTO PEDIDOS (IDCLIENTE, IDLIVRO, DATAPEDIDO, STATUS) VALUES (?, ?, CURDATE(), ?)";
 
         PreparedStatement preparedStatement = null;
 
@@ -19,6 +19,7 @@ public class PedidoDAO {
             preparedStatement = Conexao.getConexao().prepareStatement(queryInsertPedido);
             preparedStatement.setInt(1, idCliente);
             preparedStatement.setInt(2, idLivro);
+            preparedStatement.setInt(3, pedido.getStatus());
 
             preparedStatement.execute();
 
@@ -32,8 +33,27 @@ public class PedidoDAO {
         }
     }
 
-    private static void atualizarPedido(int idPedido){
+    public static void criarPedidoDevolucao(int idCliente, int idLivro){
+        String queryInsertPedido = "INSERT INTO PEDIDOS (IDCLIENTE, IDLIVRO, DATAPEDIDO, STATUS) VALUES (?, ?, CURDATE(), ?)";
 
+        PreparedStatement preparedStatement = null;
+
+        try{
+            preparedStatement = Conexao.getConexao().prepareStatement(queryInsertPedido);
+            preparedStatement.setInt(1, idCliente);
+            preparedStatement.setInt(2, idLivro);
+            preparedStatement.setInt(3, 0);
+
+            preparedStatement.execute();
+
+            preparedStatement.close();
+
+            System.out.println("Pedido de devolução feito com sucesso!");
+
+        } catch(SQLException e){
+            System.out.println("Não foi possivel fazer o pedido de devolução: ");
+            e.printStackTrace();
+        }
     }
 
 }
