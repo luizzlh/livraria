f132dbe42c6f0ee776e12ade37f92ab58132415b
Luiz <luizzlh2004@gmail.com>
2025-10-07 22:02:28 -0300

    Inicio da criação da classe livraria, primeiro método de registro de livro criado

--- src/main/java/br/com/livraria/Livraria.java
+++ src/main/java/br/com/livraria/Livraria.java
@@ -1,14 +1,43 @@
 package br.com.livraria;
 
+import br.com.DAO.ClienteDAO;
+import br.com.DAO.LivroDAO;
+import br.com.DAO.PedidoDAO;
 import br.com.entidades.Cliente;
 import br.com.entidades.Livro;
 import br.com.entidades.Pedido;
 
+import java.util.Scanner;
+
 public class Livraria {
 
-    private Livro livro;
-    private Cliente cliente;
-    private Pedido pedido;
+    private static Livro livro = new Livro();
+    private static Cliente cliente;
+    private static Pedido pedido;
+    private static LivroDAO livroDAO;
+    private static ClienteDAO clienteDAO;
+    private static PedidoDAO pedidoDAO;
+    private static Scanner scanner = new Scanner(System.in);
+
+    public static void menuCadastro(){
+        System.out.println("------------------");
+        System.out.println("Digite o nome do livro: ");
+        String tituloLivro = scanner.nextLine();
+        System.out.println("Digite o nome do autor: ");
+        String nomeAutorLivro = scanner.nextLine();
+
+        livro.setNome(tituloLivro);
+        livro.setAutor(nomeAutorLivro);
+
+        try{
+            LivroDAO.cadastrarLivro(livro);
+            Main.menu();
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+
 
 
 
