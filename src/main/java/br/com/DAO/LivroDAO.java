8336c15c02e39f5a19c11ab0a75e745273eb9da4
Luiz <luizzlh2004@gmail.com>
2025-10-21 21:32:53 -0300

    Corrigido método de verificar existência de livro, ajustes para manter código limpo e adicionado exceptions

--- src/main/java/br/com/DAO/LivroDAO.java
+++ src/main/java/br/com/DAO/LivroDAO.java
@@ -1,28 +1,62 @@
 package br.com.DAO;
 
+import br.com.Exceptions.LivroExistenteException;
 import br.com.Repositorio.Conexao;
 import br.com.entidades.Livro;
 
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.SQLOutput;
 
 public class LivroDAO {
+
+    public static void verificaLivroExistente(Livro livro){
+
+        String queryVerificaLivroExistente = "SELECT * FROM LIVROS WHERE NOME = ?";
+        PreparedStatement preparedStatementSelect = null;
+        ResultSet resultSet = null;
+
+        try{
+            preparedStatementSelect = Conexao.getConexao().prepareStatement(queryVerificaLivroExistente);
+            preparedStatementSelect.setString(1, livro.getNome());
+            resultSet = preparedStatementSelect.executeQuery();
+
+            if(resultSet.next()){
+                throw new LivroExistenteException("Livro existente!");
+            }else{
+                cadastrarLivro(livro);
+            }
+        }catch (Exception e){
+            System.out.println(e.getMessage());
+        }finally {
+            try {
+                if (resultSet != null) {
+                    resultSet.close();
+                }
+                if (preparedStatementSelect != null) {
+                    preparedStatementSelect.close();
+                }
+            } catch (java.sql.SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 
     public static void cadastrarLivro(Livro livro){
 
         String queryCadastraLivro = "INSERT INTO LIVROS (AUTOR, NOME, STATUS) VALUES (?, ?, ?)";
-
-        PreparedStatement preparedStatement = null;
+        PreparedStatement preparedStatementInsert = null;
 
         try{
-            preparedStatement = Conexao.getConexao().prepareStatement(queryCadastraLivro);
-            preparedStatement.setString(1, livro.getAutor());
-            preparedStatement.setString(2, livro.getNome());
-            preparedStatement.setInt(3, livro.getStatus());
+            preparedStatementInsert = Conexao.getConexao().prepareStatement(queryCadastraLivro);
+            preparedStatementInsert.setString(1, livro.getAutor());
+            preparedStatementInsert.setString(2, livro.getNome());
+            preparedStatementInsert.setInt(3, livro.getStatus());
 
-            preparedStatement.execute();
+            preparedStatementInsert.execute();
 
-            preparedStatement.close();
+            preparedStatementInsert.close();
 
             System.out.println("Livro cadastrado com sucesso!");
 
